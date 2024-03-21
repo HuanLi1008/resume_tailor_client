fe43dce7732b900c57fda4f08a0f4400db3960c8
Huan Li <huanli01008@gmail.com>
2024-03-21 14:09:42 -0400

    add styles for header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,10 @@ import { Link, NavLink } from "react-router-dom";
 export default function Header(){
     return(
         <nav className="nav-bar">
-            <Link className="nav-bar__logo" to={"/"}><img src={logo} alt="resume tailor logo"></img></Link>
+            <Link className="nav-bar__logo" to={"/"}><img className="nav-bar__logo--img" src={logo} alt="resume tailor logo"></img></Link>
             <div className="nav-bar__link">
-                <NavLink to={"/resume"}>My Resume</NavLink>
-                <NavLink to={"/tailor"}>My Tailor</NavLink>
+                <NavLink className="nav-bar__link--body" to={"/resume"}>My Resume</NavLink>
+                <NavLink className="nav-bar__link--body" to={"/tailor"}>My Tailor</NavLink>
             </div>
         </nav>
     )
