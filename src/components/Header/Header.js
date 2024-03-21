6a5113b2f355dcccbe87307184dc2021e6bbab15
Huan Li <huanli01008@gmail.com>
2024-03-21 13:44:30 -0400

    set up dom structure of header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,14 @@
-
+import "./Header.scss";
+import logo from '../../assets/logos/logo.png';
+import { Link, NavLink } from "react-router-dom";
 export default function Header(){
     return(
-        <nav>
-            <h1>This is a header</h1>
+        <nav className="nav-bar">
+            <Link className="nav-bar__logo" to={"/"}><img src={logo} alt="resume tailor logo"></img></Link>
+            <div className="nav-bar__link">
+                <NavLink to={"/resume"}>My Resume</NavLink>
+                <NavLink to={"/tailor"}>My Tailor</NavLink>
+            </div>
         </nav>
     )
 }
