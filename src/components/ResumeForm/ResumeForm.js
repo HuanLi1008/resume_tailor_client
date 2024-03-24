5e02004a360be7fa6471b185ff1b21d42fc92e67
Huan Li <huanli01008@gmail.com>
2024-03-24 14:28:31 -0400

    set up resume page and check if user already create username

---
+++ src/components/ResumeForm/ResumeForm.js
@@ -0,0 +1,13 @@
+
+export default function ResumeForm(){
+
+    return(
+        <section>
+            <form className="resume-form">
+                <div className="resume-form__info">
+                    <h2>Basic Info</h2>
+                </div>
+            </form>
+        </section>
+    )
+}
--- src/pages/ResumePage/ResumePage.js
+++ src/pages/ResumePage/ResumePage.js
@@ -1,12 +1,39 @@
+import { Link } from "react-router-dom";
 import "./ResumePage.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ResumeForm from "../../components/ResumeForm/ResumeForm";
 export default function ResumePage(){
+    const [resumedata, setResumedata] = useState(null);
     const user_id = localStorage.getItem("user_id");
+    
+    useEffect(()=>{
+        if(!user_id) return;
+        const fetchResume = async()=>{
+            const url = process.env.REACT_APP_API_URL;
+            try {
+                const response = await axios.get(url+"/api/resume/" + user_id);
+                if(response.data.message !== "no resume"){
+                    setResumedata(response.data);
+                }else{
+                    setResumedata(null);
+                }
+            } catch (error) {
+                console.error("Can not fetch resume", error);
+            }
+        }
+        fetchResume();
+    })
     if(user_id === null){
         return(
             <section>
                 <h1>You have to create a username first!</h1>
+                <Link className="error" to={"/user"}>Clieck Here to Create a Username</Link>
             </section>
         )
+    }
+    if(!resumedata){
+        return <ResumeForm />;
     }
     return(
         <main>
