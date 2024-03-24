d8daa4644e2de87e72732fb09504d59d3ce9494d
Huan Li <huanli01008@gmail.com>
2024-03-23 23:19:36 -0400

    add validation for creating username

--- src/pages/CreateUserPage/CreateUserPage.js
+++ src/pages/CreateUserPage/CreateUserPage.js
@@ -1,16 +1,28 @@
 import axios from "axios";
 import "./CreateUserPage.scss";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 export default function CreateUserPage(){
+    const [isValid, setIsValid] = useState(true);
     const navigate = useNavigate();
     const createUser = (e)=>{
         e.preventDefault();
+        setIsValid(true)
+        if(!e.target.username.value){
+            setIsValid(false);
+            return;
+        }
         const postUser = async()=>{
             const url = process.env.REACT_APP_API_URL;
-            const response = await axios.post(url + "/api/user", {username: e.target.username.value});
+            try {
+                const response = await axios.post(url + "/api/user", {username: e.target.username.value});
             
-            localStorage.setItem("user_id", response.data[0].id);
-            navigate("/resume");
+                localStorage.setItem("user_id", response.data.id);
+                navigate("/resume");
+            } catch (error) {
+                console.error("Can not create user: ", error);
+            }
+            
         }
         postUser();
     }
@@ -19,6 +31,7 @@ export default function CreateUserPage(){
             <form className="user__form" onSubmit={createUser}>
                 <label className="user__label" htmlFor="username">Type your username: </label>
                 <input id="username" name="username" placeholder="username" className="user__input"></input>
+                {!isValid && <div className="error user__error">Please enter your username!</div>}
                 <button className="user__btn">Create</button>
             </form>
         </section>
