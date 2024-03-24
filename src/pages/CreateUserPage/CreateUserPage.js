c2ce92220bb7f2099320a1ed372835745f064bb0
Huan Li <huanli01008@gmail.com>
2024-03-23 21:31:44 -0400

    add creste user function to handle submit of user form

--- src/pages/CreateUserPage/CreateUserPage.js
+++ src/pages/CreateUserPage/CreateUserPage.js
@@ -1,9 +1,22 @@
+import axios from "axios";
 import "./CreateUserPage.scss";
+import { useNavigate } from "react-router-dom";
 export default function CreateUserPage(){
-
+    const navigate = useNavigate();
+    const createUser = (e)=>{
+        e.preventDefault();
+        const postUser = async()=>{
+            const url = process.env.REACT_APP_API_URL;
+            const response = await axios.post(url + "/api/user", {username: e.target.username.value});
+            
+            localStorage.setItem("user_id", response.data[0].id);
+            navigate("/resume");
+        }
+        postUser();
+    }
     return(
         <section className="user">
-            <form className="user__form">
+            <form className="user__form" onSubmit={createUser}>
                 <label className="user__label" htmlFor="username">Type your username: </label>
                 <input id="username" name="username" placeholder="username" className="user__input"></input>
                 <button className="user__btn">Create</button>
