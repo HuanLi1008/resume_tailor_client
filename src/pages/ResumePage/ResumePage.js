a869ff6f48cc17f87405a14d2cd34985a50d246c
Huan Li <huanli01008@gmail.com>
2024-03-23 23:39:49 -0400

    check if user created username from resume page

--- src/pages/ResumePage/ResumePage.js
+++ src/pages/ResumePage/ResumePage.js
@@ -1,5 +1,13 @@
-
+import "./ResumePage.scss";
 export default function ResumePage(){
+    const user_id = localStorage.getItem("user_id");
+    if(user_id === null){
+        return(
+            <section>
+                <h1>You have to create a username first!</h1>
+            </section>
+        )
+    }
     return(
         <main>
             "This is resume page"
