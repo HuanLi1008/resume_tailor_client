efe590695f5b64aacd6d98c307eb4400a3378fde
Huan Li <huanli01008@gmail.com>
2024-03-23 20:46:44 -0400

    add dom structure of user form

--- src/pages/CreateUserPage/CreateUserPage.js
+++ src/pages/CreateUserPage/CreateUserPage.js
@@ -1,6 +1,13 @@
 import "./CreateUserPage.scss";
 export default function CreateUserPage(){
+
     return(
-        <h1>This is user page</h1>
+        <section className="user">
+            <form className="user__form">
+                <label className="user__label" htmlFor="username">Type your username: </label>
+                <input id="username" name="username" placeholder="username"></input>
+                <button className="user__btn">Create</button>
+            </form>
+        </section>
     )
 }
