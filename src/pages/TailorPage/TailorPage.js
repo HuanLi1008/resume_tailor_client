7de9ce359f506fa4ae7cd25e7a3804a7ee24c2e4
Huan Li <huanli01008@gmail.com>
2024-03-27 13:03:57 -0400

    check if user create a username and upload reusme at tailor page

--- src/pages/TailorPage/TailorPage.js
+++ src/pages/TailorPage/TailorPage.js
@@ -1,10 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./TailorPage.scss";
 import KeywordsPanel from "../../components/KeywordsPanel/KeywordsPanel";
 import JDinput from "../../components/JDinput/JDinput";
 import DisplayResume from "../../components/DisplayResume/DisplayResume";
+import { Link } from "react-router-dom";
+import axios from "axios";
 export default function TailorPage(){
     const [tailoring, setTailoring] = useState(false);
+
+    const user_id = localStorage.getItem("user_id");
+    const [hasResume, setHasResume] = useState(false);
+    useEffect(()=>{
+        if(!user_id) return;
+        const fetchResume = async()=>{
+            const url = process.env.REACT_APP_API_URL;
+            try {
+                const response = await axios.get(url+"/api/resume/" + user_id);
+                if(response.data.message !== "no resume"){
+                    setHasResume(true)
+                }else{
+                    setHasResume(false);
+                }
+            } catch (error) {
+                console.error("Can not fetch resume", error);
+            }
+        }
+        fetchResume();
+    })
+    if(user_id === null){
+        return(
+            <section>
+                <h1>You have to create a username first!</h1>
+                <Link className="error" to={"/user"}>Clieck Here to Create a Username</Link>
+            </section>
+        )
+    }
+    if(!hasResume){
+        return (
+            <section>
+                <h1>You haven't upload your resume!</h1>
+                <Link className="error" to={"/resume"}>Clieck Here to Upload your Resume</Link>
+            </section>
+        )
+    }
+
+
     return(
         <section className="tailor">
             <h1 className="tailor__title">Let's Tailor your Resume</h1>
