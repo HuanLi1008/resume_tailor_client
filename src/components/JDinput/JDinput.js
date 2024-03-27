1bdd182092bfd80c48b2c4035835a4103def806c
Huan Li <huanli01008@gmail.com>
2024-03-27 13:50:59 -0400

    create dom structure of jd input form

--- src/components/JDinput/JDinput.js
+++ src/components/JDinput/JDinput.js
@@ -1,9 +1,11 @@
-
+import "./JDinput.scss";
 export default function JDinput({}){
 
     return(
-        <div>
-            JDinput
-        </div>
+        <form className="job-description">
+            <h1 className="job-description__title">Target Job Description👇</h1>
+            <textarea className="job-description__input" placeholder="Job Description" name="jd"></textarea>
+            <button className="job-description__btn" type="submit">Start Tailor</button>
+        </form>
     )
 }
