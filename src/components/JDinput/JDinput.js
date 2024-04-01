4d373b5b5159865da0967b1f91afc62a9eb087d8
Huan Li <huanli01008@gmail.com>
2024-03-31 22:36:28 -0400

    add error message at jd input and style it

--- src/components/JDinput/JDinput.js
+++ src/components/JDinput/JDinput.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./JDinput.scss";
 export default function JDinput({handleSubmit}){
-
+    const [error, setError] = useState(null);
+    const handleClick = (e)=>{
+        e.preventDefault();
+        if(!e.target.jd.value){
+            setError("Please provide a job description");
+            return;
+        }
+        handleSubmit(e);
+    }
     return(
-        <form className="job-description" onSubmit={handleSubmit}>
+        <form className="job-description" onSubmit={handleClick}>
             <h2 className="job-description__title">Target Job Description👇</h2>
             <textarea className="job-description__input" placeholder="Job Description" name="jd"></textarea>
+            {error && <p className="error job-description__error">{error}</p>}
             <button className="job-description__btn" type="submit">Start Tailor</button>
         </form>
     )
